f6aa9364f5dfaadcb6aef9a018d22dd3b6131597
Lotte700 <139039799+Lotte700@users.noreply.github.com>
2023-07-22 16:15:23 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
-  import { getFirestore } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
+  import { getFirestore , collection, getDocs} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
  
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,4 +21,13 @@
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);
+
+  function getCutomers(db){
+    const cusCol = collection(db,'customers')
+    const cusSnapshot = await getDocs(cusCol)
+    return cusSnapshot
+  }
+
+const data = await getCutomers(db)
+console.log(data)
   
