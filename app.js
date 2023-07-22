877e8f70e9330b4cba6335f3b146f9b89f421d00
Lotte700 <139039799+Lotte700@users.noreply.github.com>
2023-07-22 16:08:48 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+  import { getFirestore } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
  
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,4 +20,5 @@
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
   
